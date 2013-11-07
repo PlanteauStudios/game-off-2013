b542ea99231fd47915173a73d0167afad7f275a7
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-06 20:12:07 -0500

    adding a control script and camera script i got from the unity tutorials

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float _speed;
+	public GUIText _count_text, _win_text;
+	private int _count;
+	
+	void Start() {
+		_count = 0;
+		SetCountText();
+		_win_text.text = "";
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal"),
+			  moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce(movement * _speed * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+		if (other.gameObject.tag == "pickup") {
+			other.gameObject.SetActive(false);
+			++_count;
+			SetCountText();
+		}
+	}
+	void SetCountText() {
+		_count_text.text = "Count: " + _count.ToString();		
+		if (_count >= 9) {
+			_win_text.text = "You Win!";	
+		}
+	}
+}
