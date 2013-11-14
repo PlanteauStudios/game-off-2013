380c84755a178de96eec3152e4555f60e70e4fd4
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-13 20:45:48 -0500

    now we have more 'pacmanny' type controls. you hit hte arrow key and it sends you in that direction at constant speed until you change directions or hit something

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,36 @@ public class PlayerController : MonoBehaviour {
 	public float _speed;
 	public GUIText _count_text, _win_text;
 	private int _count;
-	
+	private KeyCode _last_key;
 	void Start() {
 		_count = 0;
 		SetCountText();
-		_win_text.text = "";
+		// _win_text.text = "";
 	}
-	
-	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal"),
-			  moveVertical = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rigidbody.AddForce(movement * _speed * Time.deltaTime);
-	}
+    void OnGUI() {
+        if (Input.anyKeyDown) {
+            KeyCode key = Event.current.keyCode;
+            if (key != _last_key) {
+                _last_key = key;
+                Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+                switch (_last_key) {
+                    case KeyCode.LeftArrow:
+                        movement.Set(-1, 0.0f, 0);
+                        break;
+                    case KeyCode.RightArrow:
+                        movement.Set(1, 0.0f, 0);
+                        break;
+                    case KeyCode.UpArrow:
+                        movement.Set(0.0f, 0.0f, 1.0f);
+                        break;
+                    case KeyCode.DownArrow:
+                        movement.Set(0.0f, 0.0f, -1.0f);
+                        break;
+                }
+                rigidbody.velocity = movement * _speed;
+            }
+        }
+    }
 	void OnTriggerEnter(Collider other) {
 		//Destroy(other.gameObject);
 		if (other.gameObject.tag == "pickup") {
@@ -27,9 +44,9 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 	void SetCountText() {
-		_count_text.text = "Count: " + _count.ToString();		
+		/*_count_text.text = "Count: " + _count.ToString();		
 		if (_count >= 9) {
 			_win_text.text = "You Win!";	
-		}
+		}*/
 	}
 }
