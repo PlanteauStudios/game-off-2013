0e7e64286a2df34d37e24145e2b52df72ae6ee74
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-17 11:08:37 -0500

    extracting some logic for movement direction and for model face direction. will move to another file soon so we can use hte same logic for AI as for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ public class PlayerController : MonoBehaviour {
 	public GUIText _count_text, _win_text;
 	private int _count;
 	private KeyCode _last_key;
+    private enum Direction { Left, Right, Up, Down };
 	void Start() {
 		_count = 0;
 		SetCountText();
-		// _win_text.text = "";
+		_win_text.text = "";
 	}
     void OnGUI() {
         Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
@@ -22,37 +23,64 @@ public class PlayerController : MonoBehaviour {
         _last_key = key;
         switch (_last_key) {
             case KeyCode.LeftArrow:
-                movement.Set(-1, 0.0f, 0);
-                rot.eulerAngles = new Vector3(270.0f,270.0f ,0.0f);
+                movement = MoveDirection(Direction.Left);
+                rot.eulerAngles = FaceDirection(Direction.Left);
                 break;
             case KeyCode.RightArrow:
-                movement.Set(1, 0.0f, 0);
-                rot.eulerAngles = new Vector3(270.0f,90.0f, 0.0f);
+                movement = MoveDirection(Direction.Right);
+                rot.eulerAngles = FaceDirection(Direction.Right);
                 break;
             case KeyCode.UpArrow:
-                movement.Set(0.0f, 0.0f, 1.0f);
-                rot.eulerAngles = new Vector3(270.0f,0.0f, 0.0f);
+                movement = MoveDirection(Direction.Up);
+                rot.eulerAngles = FaceDirection(Direction.Up);
                 break;
             case KeyCode.DownArrow:
-                movement.Set(0.0f, 0.0f, -1.0f);
-                rot.eulerAngles = new Vector3(270.0f,180.0f, 0.0f);
+                movement = MoveDirection(Direction.Down);
+                rot.eulerAngles = FaceDirection(Direction.Down);
                 break;
         }
         rigidbody.velocity = movement * _speed;
         rigidbody.rotation = rot;
     }
+    Vector3 MoveDirection(Direction d) {
+        switch (d) {
+            case Direction.Left :
+                return new Vector3(-1, 0.0f, 0);
+            case Direction.Right :
+                return new Vector3(1, 0.0f, 0);
+            case Direction.Up :
+                return new Vector3(0.0f, 0.0f, 1.0f);
+            case Direction.Down :
+                return new Vector3(0.0f, 0.0f, -1.0f);
+        }
+        return new Vector3(0.0f, 0.0f, 0.0f);
+    }
+    Vector3 FaceDirection(Direction d) {
+        switch (d) {
+            case Direction.Left :
+                return new Vector3(270.0f,270.0f ,0.0f);
+            case Direction.Right :
+                return new Vector3(270.0f,90.0f, 0.0f);
+            case Direction.Up :
+                return new Vector3(270.0f,0.0f, 0.0f);
+            case Direction.Down :
+                return new Vector3(270.0f,180.0f, 0.0f);
+        }
+        return new Vector3(270.0f, 0.0f, 0.0f);
+    }
+
 	void OnTriggerEnter(Collider other) {
-		//Destroy(other.gameObject);
 		if (other.gameObject.tag == "pickup") {
 			other.gameObject.SetActive(false);
 			++_count;
 			SetCountText();
 		}
 	}
+
 	void SetCountText() {
-		/*_count_text.text = "Count: " + _count.ToString();		
+		_count_text.text = "Count: " + _count.ToString();		
 		if (_count >= 9) {
 			_win_text.text = "You Win!";	
-		}*/
+		}
 	}
 }
