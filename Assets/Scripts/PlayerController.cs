9008d0066f5713b67ab3e759ce954ec74d74f780
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-17 12:15:21 -0500

    forgot this in the last commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour {
 	public GUIText _count_text, _win_text;
 	private int _count;
 	private KeyCode _last_key;
-    private enum Direction { Left, Right, Up, Down };
+    private Movement _movement_script;
+
+
 	void Start() {
 		_count = 0;
 		SetCountText();
 		_win_text.text = "";
+        _movement_script = GetComponent<Movement>();
 	}
     void OnGUI() {
         Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
@@ -23,51 +26,26 @@ public class PlayerController : MonoBehaviour {
         _last_key = key;
         switch (_last_key) {
             case KeyCode.LeftArrow:
-                movement = MoveDirection(Direction.Left);
-                rot.eulerAngles = FaceDirection(Direction.Left);
+                movement = _movement_script.MoveDirection(Movement.Direction.Left);
+                rot.eulerAngles = _movement_script.FaceDirection(Movement.Direction.Left);
                 break;
             case KeyCode.RightArrow:
-                movement = MoveDirection(Direction.Right);
-                rot.eulerAngles = FaceDirection(Direction.Right);
+                movement = _movement_script.MoveDirection(Movement.Direction.Right);
+                rot.eulerAngles = _movement_script.FaceDirection(Movement.Direction.Right);
                 break;
             case KeyCode.UpArrow:
-                movement = MoveDirection(Direction.Up);
-                rot.eulerAngles = FaceDirection(Direction.Up);
+                movement = _movement_script.MoveDirection(Movement.Direction.Up);
+                rot.eulerAngles = _movement_script.FaceDirection(Movement.Direction.Up);
                 break;
             case KeyCode.DownArrow:
-                movement = MoveDirection(Direction.Down);
-                rot.eulerAngles = FaceDirection(Direction.Down);
+                movement = _movement_script.MoveDirection(Movement.Direction.Down);
+                rot.eulerAngles = _movement_script.FaceDirection(Movement.Direction.Down);
                 break;
         }
         rigidbody.velocity = movement * _speed;
         rigidbody.rotation = rot;
     }
-    Vector3 MoveDirection(Direction d) {
-        switch (d) {
-            case Direction.Left :
-                return new Vector3(-1, 0.0f, 0);
-            case Direction.Right :
-                return new Vector3(1, 0.0f, 0);
-            case Direction.Up :
-                return new Vector3(0.0f, 0.0f, 1.0f);
-            case Direction.Down :
-                return new Vector3(0.0f, 0.0f, -1.0f);
-        }
-        return new Vector3(0.0f, 0.0f, 0.0f);
-    }
-    Vector3 FaceDirection(Direction d) {
-        switch (d) {
-            case Direction.Left :
-                return new Vector3(270.0f,270.0f ,0.0f);
-            case Direction.Right :
-                return new Vector3(270.0f,90.0f, 0.0f);
-            case Direction.Up :
-                return new Vector3(270.0f,0.0f, 0.0f);
-            case Direction.Down :
-                return new Vector3(270.0f,180.0f, 0.0f);
-        }
-        return new Vector3(270.0f, 0.0f, 0.0f);
-    }
+
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "pickup") {
