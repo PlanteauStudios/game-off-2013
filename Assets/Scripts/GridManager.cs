c80f77e7f548a42971649c4ba61580200381d660
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-21 06:45:03 -0500

    Changes to all the calculations to deal with squares instead of hexagons.

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -21,20 +21,13 @@ public class GridManager: MonoBehaviour
     }
  
     //The method used to calculate the number cells in a row and number of rows
-    //Vector2.x is gridWidthInHexes and Vector2.y is gridHeightInCells
+    //Vector2.x is gridWidthInCells and Vector2.y is gridHeightInCells
     Vector2 calcGridSize()
     {
         float sideLength = cellHeight;
         //the number of whole cell sides that fit inside inside ground height
         int nrOfSides = (int)(groundHeight / sideLength);
-        //I will not try to explain the following caculation because I made some assumptions, which might not be correct in all cases, to come up with the formula. So you'll have to trust me or figure it out yourselves.
-        int gridHeightInCells = nrOfSides;//(int)( nrOfSides * 2 / 3);
-        //When the number of cells is even the tip of the last cell in the offset column might stick up.
-        //The number of cells in that case is reduced.
-        if (gridHeightInCells % 2 == 0
-            && (nrOfSides + 0.5f) * sideLength > groundHeight)
-            gridHeightInCells--;
-        //gridWidth in cells is calculated by simply dividing ground width by cell width
+        int gridHeightInCells = nrOfSides;
         return new Vector2((int)(groundWidth / cellWidth), gridHeightInCells);
     }
     //Method to calculate the position of the first tile
@@ -52,11 +45,9 @@ public class GridManager: MonoBehaviour
     {
         Vector3 initPos = calcInitPos();
         float offset = 0;
-        if (gridPos.y % 2 != 0)
-            offset = cellWidth / 2;
  
         float x =  initPos.x + offset + gridPos.x * cellWidth;
-        float z = initPos.z - gridPos.y * cellHeight * 0.75f;
+        float z = initPos.z - gridPos.y * cellHeight;
         //If your ground is not a plane but a cube you might set the y coordinate to sth like groundDepth/2 + hexDepth/2
         return new Vector3(x, 0, z);
     }
@@ -69,9 +60,6 @@ public class GridManager: MonoBehaviour
         for (float y = 0; y < gridSize.y; y++)
         {
             float sizeX = gridSize.x;
-            //if the offset row sticks up, reduce the number of cells in a row
-            if (y % 2 != 0 && (gridSize.x + 0.5) * cellWidth > groundWidth)
-                sizeX--;
             for (float x = 0; x < sizeX; x++)
             {
                 GameObject cell = (GameObject)Instantiate(Cell);
