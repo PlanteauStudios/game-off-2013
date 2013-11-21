2bb4798ba125cdfa18b16e63da758204311f351a
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-21 06:46:43 -0500

    simple ai script to get pacman to track the player

---
+++ Assets/Scripts/PacManAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacManAI : MonoBehaviour {
+    enum State { Explore, Hunt, Hide };
+    private State _state;
+    public float _speed;
+    private Movement _movement_script;
+    private Movement.Direction _direction;
+    public GameObject _player;
+	// Use this for initialization
+	void Start () {
+        _state = State.Explore;
+        _movement_script = GetComponent<Movement>();
+        _direction = Movement.Direction.Up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 player_pos = _player.rigidbody.position;
+
+        Vector3 direct = (player_pos - rigidbody.position) * .25f;
+        _direction = _movement_script.MovingIn(direct);
+        rigidbody.velocity = _movement_script.MoveDirection(_direction) * _speed;
+        Quaternion rot = Quaternion.identity;
+        rot.eulerAngles = _movement_script.FaceDirection(_direction);
+        rigidbody.rotation = rot;
+	}
+}
