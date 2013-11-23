cf7d87365cacdc0ce53ae358b78e7563049767ff
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-23 09:11:22 -0500

    just some cleaning of unsused stuff

--- Assets/Scripts/GhostAI.cs
+++ Assets/Scripts/GhostAI.cs
@@ -14,14 +14,10 @@ public class GhostAI : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-        // Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
         Quaternion rot = Quaternion.identity;
         rot.eulerAngles = new Vector3(270.0f, 0.0f, 0.0f);
         Vector3 direct = _pacman.rigidbody.position - rigidbody.position;
 
-        Vector3 horizontal = new Vector3(direct.x, 0.0f, 0.0f), 
-                vertical = new Vector3(0.0f, 0.0f, direct.z);
-        Vector3 movement = horizontal.x < vertical.z ? horizontal : vertical;
         _direction = _movement_script.MovingIn(direct);
         rigidbody.velocity = _movement_script.MoveDirection(_direction) * _speed;
         rot.eulerAngles = _movement_script.FaceDirection(_direction);
