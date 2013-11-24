df5f83d4b018f26c99eaf11a21ab6558edb90c65
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-24 11:07:43 -0500

    going to redo this a bit, need a way to determine if a cell is underneath / touching a wall

---
+++ Assets/Scripts/CellPassthrough.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellPassthrough : MonoBehaviour {
+    private bool _passable = true;
+	
+    void OnTriggerEnter(Collider other) {
+        if (_passable && other.gameObject.tag == "Wall") {
+            _passable = false;
+            Debug.Log("Not a _passable cell");
+        }
+    }
+    public bool CanPass() { return _passable; }
+}
