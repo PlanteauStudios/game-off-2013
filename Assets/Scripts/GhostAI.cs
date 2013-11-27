8051c720bfaa22057feb19a173f81a8962a08167
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-27 18:40:03 -0500

    so we're not just picking random directions when we hit a wall. makes the ai dumber (for now) but it was a bit easier to implement and the deadline for the contest is in...not long :(

--- Assets/Scripts/GhostAI.cs
+++ Assets/Scripts/GhostAI.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GhostAI : MonoBehaviour {
@@ -7,7 +7,6 @@ public class GhostAI : MonoBehaviour {
     private Movement.Direction _direction;
     public GameObject _pacman, _floor;
 
-    // Use this for initialization
     void Start () {
         _movement_script = GetComponent<Movement>();
         _direction = Movement.Direction.Up;
@@ -16,27 +15,47 @@ public class GhostAI : MonoBehaviour {
         int ran = Random.Range(0, 3);
         return ran;
     }
+    bool CollisionIn(Movement.Direction dir) {
+        Vector3 attempted_direction = Movement.MoveDirection(dir);
+        Ray ray = new Ray(transform.position, attempted_direction);
+        RaycastHit hit;
+        Bounds b = collider.bounds;
+
+        if (Physics.Raycast(ray, out hit,5f)) {
+            return true;
+        }
+        return false;
+    }
+    bool Reverseing(Movement.Direction before, Movement.Direction after) {
+        bool ret = false;
+        if ((before == Movement.Direction.Up && after == Movement.Direction.Down) ||
+            (before == Movement.Direction.Left && after == Movement.Direction.Right))
+            ret = true;
+        if ((after == Movement.Direction.Up && before == Movement.Direction.Down) ||
+             (after == Movement.Direction.Left && before == Movement.Direction.Right))
+            ret = true;
+        return ret;
+    }
     void SwitchDirection(int dir) {
         if (dir > 3) dir = 0;
         Movement.Direction next_direction = (Movement.Direction)dir;//_direction;
 
-        if (next_direction == _direction)
+        if (next_direction == _direction || Reverseing(_direction, next_direction) ||
+            CollisionIn(next_direction)) {
             SwitchDirection(dir + 1);
-        else {
+        } else {
             _direction = next_direction;
         }
     }
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Floor") return;
-    
+    void OnCollisionEnter(Collision other) {
+        if (other.collider.gameObject.tag == "Floor") return;
         SwitchDirection(Randomize());
     }
-    // Update is called once per frame
     void Update () {
         Quaternion rot = Quaternion.identity;
         rot.eulerAngles = new Vector3(270.0f, 0.0f, 0.0f);
-        rigidbody.velocity = _movement_script.MoveDirection(_direction) * _speed;
-        rot.eulerAngles = _movement_script.FaceDirection(_direction);
+        rigidbody.velocity = Movement.MoveDirection(_direction) * _speed;
+        rot.eulerAngles = Movement.FaceDirection(_direction);
         rigidbody.rotation = rot;
     }
 }
