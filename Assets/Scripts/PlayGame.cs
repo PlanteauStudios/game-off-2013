cbdbfdeb6dc8d77a202547cc09869e7341e12ea9
Graham Saulnier <grahamsaulnier@gmail.com>
2013-11-30 07:44:38 -0500

    simpel script to start up game from menu

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGame : MonoBehaviour {
+    public Texture btnTexture;
+    void OnGUI() {
+        if (Input.anyKeyDown)
+            Application.LoadLevel(1);
+    }
+    void OnMouseDown() {
+        Application.LoadLevel(1);
+    }
+}
